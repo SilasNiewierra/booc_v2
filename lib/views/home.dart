41f496a995397cc38e136333f6f65c77a50adf11
Silas <niewierra.silas@gmail.com>
2020-12-01 16:30:38 +0100

    updated sign out design

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:booc/services/authenticate.dart';
+import 'package:booc/services/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,10 +7,18 @@ class HomePage extends StatelessWidget {
       new AuthenticationService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: RaisedButton(onPressed: () {
-        authenticationService.signOut();
-      }),
+    return Scaffold(
+        body: Center(
+      child: RaisedButton(
+          onPressed: () {
+            authenticationService.signOut();
+          },
+          child: Text(
+            "Sign Out",
+            style: Theme.of(context).textTheme.headline6.copyWith(
+                  color: ColorService().getLightTextColor(),
+                ),
+          )),
     ));
   }
 }
