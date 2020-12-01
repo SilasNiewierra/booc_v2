a4e8c46715dc4a87c8980e30a3a5c2c10775e011
Silas <niewierra.silas@gmail.com>
2020-12-01 16:09:38 +0100

    added global responsive theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:booc/services/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -30,22 +31,55 @@ class MyApp extends StatelessWidget {
         // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
           return MultiProvider(
-              providers: [
-                // Make user stream available
-                StreamProvider<User>.value(value: AuthenticationService().user),
-              ],
+            providers: [
+              // Make user stream available
+              StreamProvider<User>.value(value: AuthenticationService().user),
+            ],
 
-              // All data will be available in this child and descendents
-              child: MaterialApp(
-                debugShowCheckedModeBanner: false,
-                title: 'booc',
-                home: Root(),
-              ));
+            // All data will be available in this child and descendents
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'booc',
+              builder: (context, child) {
+                return new Theme(
+                  data: _boocTheme(MediaQuery.of(context).size.height),
+                  child: child,
+                );
+              },
+              home: Root(),
+            ),
+          );
         }
 
         // Otherwise, show something whilst waiting for initialization to complete
         return Scaffold(body: Text("Loading Firebase ..."));
       },
+    );
+  }
+
+  ThemeData _boocTheme(double height) {
+    return ThemeData(
+      primaryColor: ColorService().getPrimaryColor(),
+      accentColor: ColorService().getPrimaryColor(),
+      buttonColor: ColorService().getPrimaryColor(),
+      textTheme: TextTheme(
+        headline1: TextStyle(
+            fontSize: height / 15, color: ColorService().getPrimaryColor()),
+        headline2: TextStyle(
+            fontSize: height / 20, color: ColorService().getPrimaryColor()),
+        headline3: TextStyle(
+            fontSize: height / 30, color: ColorService().getPrimaryColor()),
+        headline4: TextStyle(
+            fontSize: height / 35, color: ColorService().getPrimaryColor()),
+        headline5: TextStyle(
+            fontSize: height / 50, color: ColorService().getPrimaryColor()),
+        headline6: TextStyle(
+            fontSize: height / 65, color: ColorService().getPrimaryColor()),
+        bodyText1: TextStyle(
+            fontSize: height / 70, color: ColorService().getPrimaryColor()),
+        bodyText2: TextStyle(
+            fontSize: height / 80, color: ColorService().getPrimaryColor()),
+      ),
     );
   }
 }
--- lib/views/singin.dart
+++ lib/views/singin.dart
@@ -1,10 +1,159 @@
+import 'package:booc/services/authenticate.dart';
+import 'package:booc/services/colors.dart';
 import 'package:flutter/material.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final AuthenticationService _authService = AuthenticationService();
+  final _formKey = GlobalKey<FormState>();
+
+  RegExp validMail = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+  String email = '';
+  String password = '';
+  String error = '';
+
   @override
   Widget build(BuildContext context) {
+    MediaQueryData queryData = MediaQuery.of(context);
     return Scaffold(
-      body: Text("While Im signing in"),
+      appBar: _buildAppBar(context),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome back!",
+                  style: Theme.of(context).textTheme.headline4,
+                  textAlign: TextAlign.start,
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  "Enter your credentials to continue",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      .copyWith(color: ColorService().getSecondaryColor()),
+                  textAlign: TextAlign.start,
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.only(top: 20), // add padding to adjust text
+                    isDense: true,
+                    hintText: "Email",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(
+                          top: 15), // add padding to adjust icon
+                      child: Icon(
+                        Icons.perm_identity_outlined,
+                        size: 30.0,
+                      ),
+                    ),
+                  ),
+                  validator: (val) => val.isNotEmpty && validMail.hasMatch(val)
+                      ? null
+                      : 'Enter an email',
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.only(top: 20), // add padding to adjust text
+                    isDense: true,
+                    hintText: "Password",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(
+                          top: 15), // add padding to adjust icon
+                      child: Icon(
+                        Icons.vpn_key_outlined,
+                        size: 30.0,
+                      ),
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (val) =>
+                      val.length < 6 ? 'Enter an password 6+ chars long' : null,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  },
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 50.0),
+                  child: SizedBox(
+                    width: queryData.size.width,
+                    child: RaisedButton(
+                        child: Text(
+                          "Sign In".toUpperCase(),
+                          style: Theme.of(context).textTheme.headline6.copyWith(
+                              color: ColorService().getLightTextColor()),
+                        ),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            dynamic result = await _authService
+                                .signInWithEmailAndPassword(email, password);
+
+                            if (result == null) {
+                              setState(() => error =
+                                  "Could not sign in with the provided credentials.");
+                            } else {
+                              print(result);
+                              Navigator.pop(context);
+                            }
+                          }
+                        }),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Center(
+                    child: Text(
+                      error,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2
+                          .copyWith(color: Colors.red),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  Widget _buildAppBar(BuildContext context) {
+    return AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Theme.of(context).primaryColor,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: [],
+        backgroundColor: Colors.transparent);
   }
 }
