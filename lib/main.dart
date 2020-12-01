f67dace00066091b46886eb5a1ace31429bcece8
Silas <niewierra.silas@gmail.com>
2020-12-01 12:52:20 +0100

    added folder structure, provider and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,11 +6,30 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'booc',
-      home: Container(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text("Something went wrong with Firebase Init");
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'booc',
+            home: Container(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Text("Loading Firebase ...");
+      },
     );
   }
 }
