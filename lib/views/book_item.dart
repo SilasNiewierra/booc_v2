dc1312d5ef72042f7dcad3f9cb194d5d33fbca45
Silas <niewierra.silas@gmail.com>
2020-12-09 07:50:49 +0100

    added bottom sheet modal

--- lib/views/book_item.dart
+++ lib/views/book_item.dart
@@ -1,7 +1,9 @@
 import 'package:booc/models/book_model.dart';
 import 'package:booc/models/variables.dart';
+import 'package:booc/services/colors.dart';
 import 'package:booc/views/detail.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class BookItem extends StatelessWidget {
   final Book book;
@@ -53,7 +55,42 @@ class BookItem extends StatelessWidget {
                     ],
                   ),
                 ),
-                IconButton(icon: Icon(Icons.more_vert), onPressed: null)
+                IconButton(
+                    icon: Icon(Icons.more_vert),
+                    onPressed: () {
+                      showMaterialModalBottomSheet(
+                        context: context,
+                        builder: (context) => SingleChildScrollView(
+                          controller: ModalScrollController.of(context),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 50.0, horizontal: 50.0),
+                            color: ColorService().getBackgroundColor(),
+                            child: Center(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  FlatButton.icon(
+                                      icon: Icon(Icons.clear),
+                                      onPressed: null,
+                                      label: Text("Remove from list",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline5)),
+                                  FlatButton.icon(
+                                      icon: Icon(Icons.favorite_outline),
+                                      onPressed: null,
+                                      label: Text("Like this book",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline5)),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    })
               ],
             ),
           ],
